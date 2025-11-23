73d3db2e6eddc339a81fc28d9fe7cfea5c2d354a
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-24 00:41:36 +0300

    Initialized Orderwith customer, coffee and price and set property for price

--- lib/order.py
+++ lib/order.py
@@ -1,2 +1,17 @@
 class Order:
-    pass
+    def __init__(self, customer, coffee, price):
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
+
+    @property
+    def price(self):
+        return self._price 
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, float):
+            raise Exception("Price must be a float.")
+        if not(1.0 <= value <= 10.0):
+            raise Exception("Price must be between 1.0 and 10.0")
+        self._price = value
+    
