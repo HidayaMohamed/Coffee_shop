ad3958a4438982984bb1df9c8ee948bab80ea96d
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-24 00:23:29 +0300

    Initialized coffee class with name and added validatons

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,2 +1,15 @@
 class Coffee:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    @property 
+    def name(self):
+        return self._name 
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("Coffee name must be a string.")
+        if not len(value)< 3:
+            raise Exception("Coffee name must be atleast 3 characters long.")
+        self._name = value
+        
