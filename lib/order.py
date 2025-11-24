b33816cb96faaac793ab54162a28aa22d2ed12ad
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-24 09:33:45 +0300

    Added validation for coffee and customer to be instances and imported Coffee and Customer classes

--- lib/order.py
+++ lib/order.py
@@ -1,8 +1,16 @@
+from .customer import Customer
+from .coffee import Coffee
+
+
 class Order:
+    all = []
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+
+
+        Order.all.append(self)
 
     @property
     def price(self):
@@ -14,4 +22,20 @@ class Order:
         if not(1.0 <= value <= 10.0):
             raise Exception("Price must be between 1.0 and 10.0")
         self._price = value
-    
+    @property
+    def customer(self):
+        return self._customer
+    @customer.setter
+    def cusomer(self, value):
+        if not isinstance(value, Customer):
+            raise Exception("customer must be a Customer instanace")
+        self._customer = value
+
+    @property
+    def coffee(self):
+        return self._coffe
+    @coffee.setter
+    def coffee(self, value):
+        if not isinstance(value, Coffee):
+            raise Exception("coffee must be a Coffee instance")
+        self._coffee = value
