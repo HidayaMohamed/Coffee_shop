60bbf59e5c936d76994ea257c6c2cf0e061d6715
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-24 10:04:45 +0300

    Imported order classs in coffee.py

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,7 +1,11 @@
+from .order import Order
+
 class Coffee:
+    all =[]
     def __init__(self, name):
         self.name = name
 
+        Coffee.all.append(self)
     @property 
     def name(self):
         return self._name 
@@ -13,3 +17,10 @@ class Coffee:
             raise Exception("Coffee name must be atleast 3 characters long.")
         self._name = value
         
+    def orders(self):
+        # Return all Order objects that include THIS coffee.
+        return [order for order in Order.all if order.coffee is self]
+
+    def customers(self):
+        # Return UNIQUE Customer objects who ordered this coffee.import .Order from order
+        return list({order.customer for order in self.orders()})
